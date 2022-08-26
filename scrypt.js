75f27c840a8d93a7932699d4128c0ffbb9ba988d
viacheslav Kurov <0990004793qw@gmail.com>
2022-08-26 17:36:58 +0300

    change FirstName < lastName

--- scrypt.js
+++ scrypt.js
@@ -1,45 +1,44 @@
 "use strict";
 class Person {
-	constructor(firstName, lastName, age, birthDayDate) {
-	  this.firstName = firstName;
-	  this.lastName = lastName;
-	  this.age = age;
-	  this.birthDayDate = new Date(birthDayDate);
-	}
-	celebrate() {
-	  console.log("Happy Birthday, let`s celebrate");
-	}
- }
- 
- class Employee extends Person {
-	#salary = 1000;
-	constructor(firstName, lastName, age, birthDayDate, jobPosition) {
-	  super(firstName, lastName, age, birthDayDate);
-	  this.jobPosition = jobPosition;
-	}
-	getYearSalary() {
-	  return this.#salary * 12;
-	}
-	checkCelebrateDay() {
-	  let day = new Date().getFullYear();
-	  this.birthDayDate.setFullYear(day);
-	  let birthDay = this.birthDayDate;
-	  function isWeekend(date) {
-		 let checkWeekend = new Date(date).getDay();
-		 return checkWeekend === 6 || checkWeekend === 0;
-	  }
-	  if (isWeekend(birthDay) === true) {
-		 console.log(super.celebrate());
-	  } else {
-		 console.log("Happy Birthday, but I need to work");
-	  }
-	}
- }
- const personInfo = new Person("oleg", "huev", 22, "1990-09-21");
- const employeeInfo = new Employee("slava", "kurov", 31, "1990-09-21");
+  constructor(firstName, lastName, age, birthDayDate) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.birthDayDate = new Date(birthDayDate);
+  }
+  celebrate() {
+    console.log("Happy Birthday, let`s celebrate");
+  }
+}
 
- console.log(employeeInfo);
- console.log(personInfo);
- console.log(employeeInfo.getYearSalary());
- employeeInfo.checkCelebrateDay();
- 
+class Employee extends Person {
+  #salary = 1000;
+  constructor(firstName, lastName, age, birthDayDate, jobPosition) {
+    super(firstName, lastName, age, birthDayDate);
+    this.jobPosition = jobPosition;
+  }
+  getYearSalary() {
+    return this.#salary * 12;
+  }
+  checkCelebrateDay() {
+    let day = new Date().getFullYear();
+    this.birthDayDate.setFullYear(day);
+    let birthDay = this.birthDayDate;
+    function isWeekend(date) {
+      let checkWeekend = new Date(date).getDay();
+      return checkWeekend === 6 || checkWeekend === 0;
+    }
+    if (isWeekend(birthDay) === true) {
+      console.log(super.celebrate());
+    } else {
+      console.log("Happy Birthday, but I need to work");
+    }
+  }
+}
+const personInfo = new Person("Oleg", "Nichaev", 22, "1990-09-21");
+const employeeInfo = new Employee("Viacheslav", "Kurov", 31, "1992-10-11");
+
+console.log(employeeInfo);
+console.log(personInfo);
+console.log(employeeInfo.getYearSalary());
+employeeInfo.checkCelebrateDay();
