c76721b386d93342b300e238f408cb0286e49468
viacheslav Kurov <0990004793qw@gmail.com>
2022-08-26 17:32:28 +0300

    Done

---
+++ scrypt.js
@@ -0,0 +1,45 @@
+"use strict";
+class Person {
+	constructor(firstName, lastName, age, birthDayDate) {
+	  this.firstName = firstName;
+	  this.lastName = lastName;
+	  this.age = age;
+	  this.birthDayDate = new Date(birthDayDate);
+	}
+	celebrate() {
+	  console.log("Happy Birthday, let`s celebrate");
+	}
+ }
+ 
+ class Employee extends Person {
+	#salary = 1000;
+	constructor(firstName, lastName, age, birthDayDate, jobPosition) {
+	  super(firstName, lastName, age, birthDayDate);
+	  this.jobPosition = jobPosition;
+	}
+	getYearSalary() {
+	  return this.#salary * 12;
+	}
+	checkCelebrateDay() {
+	  let day = new Date().getFullYear();
+	  this.birthDayDate.setFullYear(day);
+	  let birthDay = this.birthDayDate;
+	  function isWeekend(date) {
+		 let checkWeekend = new Date(date).getDay();
+		 return checkWeekend === 6 || checkWeekend === 0;
+	  }
+	  if (isWeekend(birthDay) === true) {
+		 console.log(super.celebrate());
+	  } else {
+		 console.log("Happy Birthday, but I need to work");
+	  }
+	}
+ }
+ const personInfo = new Person("oleg", "huev", 22, "1990-09-21");
+ const employeeInfo = new Employee("slava", "kurov", 31, "1990-09-21");
+
+ console.log(employeeInfo);
+ console.log(personInfo);
+ console.log(employeeInfo.getYearSalary());
+ employeeInfo.checkCelebrateDay();
+ 
